7ee58b38cf450e4739fcf2259015f66731c2a8e1
bjl0615 <bjl0615@gmail.com>
2023-09-27 00:41:09 +0900

    Spring JDBCTemplate

--- src/main/java/hello/hellospring/repository/JdbcMemberRepository.java
+++ src/main/java/hello/hellospring/repository/JdbcMemberRepository.java
@@ -16,26 +16,6 @@ public class JdbcMemberRepository implements MemberRepository {
     private final DataSource dataSource;
     public JdbcMemberRepository(DataSource dataSource) {
         this.dataSource = dataSource;
-
-        String sql = "insert into member(name) values(?)";
-
-        Connection conn = null;
-        PreparedStatement pstmt = null;
-        ResultSet rs = null;
-
-        try {
-            conn = getConnection();
-            pstmt = conn.prepareStatement(sql,
-                    Statement.RETURN_GENERATED_KEYS);
-            pstmt.setString(1, "spring3");
-            pstmt.executeUpdate();
-            rs = pstmt.getGeneratedKeys();
-        } catch (Exception e) {
-            throw new IllegalStateException(e);
-        } finally {
-            close(conn, pstmt, rs);
-        }
-
     }
     @Override
     public Member save(Member member) {
